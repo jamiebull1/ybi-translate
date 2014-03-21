dd9dc733efacfa22825b808e9a56daaf775713fa
botsofbitcoin <jamie@botsofbitcoin.com>
2014-03-21 07:10:00 +0000

    Added localisation to manifest.json and _locales/ files for en and es

--- background.js
+++ background.js
@@ -7,23 +7,28 @@ var API_KEY = "AIzaSyAkVsXAEIcnilWTDg-32iYDjhvP07tH1lc";
 function translateRequest(selection) {
 	chrome.storage.sync.get("targetLanguage", function(stored){
 		// Build the URL
-		var apiAndKey = API + "key=" + API_KEY;
-		var options = "&format=text";
-		var query = "&q=" + encodeURIComponent(selection);
-		var targetLanguage = stored.targetLanguage;
-		if (!targetLanguage){
-			targetLanguage = "en";
-		}
-		var target = "&target=" + targetLanguage;
-		var url = apiAndKey + target + options + query;
-		// Make the request
-		var xmlhttp;
-		xmlhttp=new XMLHttpRequest();
-		xmlhttp.open("GET", url, false);
-		xmlhttp.send();
-		response = JSON.parse(xmlhttp.responseText);
-		// Show the result
-		alert(selection + ":\n\n" + response.data.translations[0].translatedText);
+		chrome.storage.local.clear();
+		if(selection){
+			var apiAndKey = API + "key=" + API_KEY;
+			var options = "&format=text";
+			var query = "&q=" + encodeURIComponent(selection);
+			var targetLanguage = stored.targetLanguage;
+			if (!targetLanguage){
+				targetLanguage = "en";
+			}
+			var target = "&target=" + targetLanguage;
+			var url = apiAndKey + target + options + query;
+			// Make the request
+			var xmlhttp;
+			xmlhttp=new XMLHttpRequest();
+			xmlhttp.open("GET", url, false);
+			xmlhttp.send();
+			response = JSON.parse(xmlhttp.responseText);
+			// Show the result
+			alert(selection + ":\n\n" + response.data.translations[0].translatedText);
+		} else {
+			alert("No text selected.");
+		}	
 	});
 }
 
